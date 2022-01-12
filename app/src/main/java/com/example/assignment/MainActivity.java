d1ff2fb6652a36e220f33bac2c398b73bf73c023
Deepak <tyron11199@gmail.com>
2022-01-13 00:08:33 +0530

    This is the material based Ui Application

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
         String[] demo = {"one","two","three"};
 
+        String[] demo1 = {"one","two","three"};
+
         RecyclerView recycle = (RecyclerView) findViewById(R.id.recycle);
         BottomNavigationView bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
